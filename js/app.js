4531ea7dff4dc02a280cd038f5ab0f3eb3d37bdb
WILLIAM GREEN <TEHBILLIS@GMAIL.COM>
2023-11-14 22:12:10 -0800

    Add initial product object constructor

---
+++ js/app.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  const imagesToDisplay = 3;
+
+  class oddProduct {
+    constructor (name, path) {
+      this.name = name;
+      this.path = path;
+      this.timesShown = 0;
+    }
+  }
+
+  function generateProducts() {
+    //TODO: for loop that loops through equal to the value of imagesToDisplay.
+  }
+}
